7c0d87fbc742354d3bfbab3cf6f3bb1023103a5b
Simon Couvreur <simoncouvreur@hotmail.com>
2019-05-28 12:47:05 +0100

    Outlier removal before transformations

--- src/transform_features.R
+++ src/transform_features.R
@@ -1,11 +1,15 @@
-transform_features <- function(raw){
+transform_features <- function(raw, ...){
   # raw: class data.table with features in columns, samples in rows
   # requires all features are in [0,1]
+  # can pass fold argument to remove_outliers_univar
   
   # assertions
   stopifnot(is.data.table(raw))
   stopifnot(all(unlist(raw[,lapply(.SD, is.numeric)])))
   stopifnot(all(unlist(raw)>=0, na.rm = TRUE) & all(unlist(raw)<=1, na.rm = TRUE))
+  
+  # remove outliers
+  raw <- remove_outliers_univar(raw, ...)
   
   # perform transformations
   for (j in names(raw)){
